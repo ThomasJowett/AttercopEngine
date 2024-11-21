2bf8de6bb29af7db84fb12f94643bf5bb7e9d363
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2024-11-21 18:03:48 +0000

    update the geometry parser to take 3D meshes

--- Attercop/src/SimpleMeshParser.cpp
+++ Attercop/src/SimpleMeshParser.cpp
@@ -1,7 +1,7 @@
 #include "SimpleMeshParser.hpp"
 namespace atcp
 {
-bool SimpleMeshParser::LoadGeometry(const std::filesystem::path &path, std::vector<float> &vertexData, std::vector<uint16_t> &indexData)
+bool SimpleMeshParser::LoadGeometry(const std::filesystem::path& path, std::vector<float>& vertexData, std::vector<uint16_t>& indexData, int dimensions)
 {
 	std::ifstream file(path);
 	if (!file.is_open())
@@ -25,36 +25,36 @@ bool SimpleMeshParser::LoadGeometry(const std::filesystem::path &path, std::vect
 	while (!file.eof())
 	{
 		getline(file, line);
-		if(!line.empty() && line.back() == 'r') {
+		if (!line.empty() && line.back() == 'r') {
 			line.pop_back();
 		}
 
-		if(line == "[vertices]") {
+		if (line == "[vertices]") {
 			currentSection = Section::Vertices;
 		}
-		else if(line == "[indices]") {
+		else if (line == "[indices]") {
 			currentSection = Section::Indices;
 		}
 		else if (line.empty() || line[0] == '#')
 		{
 			continue;
 		}
-		else if(currentSection == Section::Vertices) {
+		else if (currentSection == Section::Vertices) {
 			std::istringstream iss(line);
-			for(int i = 0; i < 5; ++i) {
+			for (int i = 0; i < dimensions + 3; ++i) {
 				iss >> value;
 				vertexData.push_back(value);
 			}
 		}
 		else if (currentSection == Section::Indices) {
 			std::istringstream iss(line);
-			for(int i = 0; i < 3; ++i) {
+			for (int i = 0; i < 3; ++i) {
 				iss >> index;
 				indexData.push_back(index);
 			}
 		}
 	}
-	
+
 	return true;
 }
 } // namespace atcp
